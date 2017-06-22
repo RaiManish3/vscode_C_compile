a80f5c4bede2a90309dd279884a98bc7fa6e938c
RaiManish3 <manish.07.1996@gmail.com>
2017-06-22 17:03:50 +0530

    highlight errors and simple warnings

---
+++ sample.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+//#include <string.h>
+
+int main()
+{
+       //char src[40];
+
+          int n;
+          scanf("%d",&n);
+            
+          char dest[n];
+             //memset(dest, '\0', sizeof(dest));
+                strcpy(src, "This is tutorialspoint.com");
+                   strcpy(dest, src);char strx[44];strcpy(strx, src);
+
+                      printf("Final copied string : %s\n", dest);
+                         
+                         return(0);
+}
